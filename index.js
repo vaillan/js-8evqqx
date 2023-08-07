8cb25a4f07f1192127a7645e0c9ff28254d3d009
vaillan <red.line.v.o.s@outlook.com>
2023-08-06 22:13:20 -0600

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+//Variables globales
+let dias = 0;
+let anio = 0;
+let mes = 0;
+
+//Procesos generales
+document.getElementById('c').addEventListener('click', onInit);
+
+/**
+ * Función para inicializar proceso principal
+ * @return {void}
+ */
+function onInit(e) {
+  anio = document.getElementById('anio').value;
+  mes = document.getElementById('mes').value;
+  const date = new Date(anio, mes, 0);
+  dias = date.getDate();
+  setAlert();
+}
+
+/**
+ * Función para mostrar etiqueta con el año, mes y días correspondientes
+ * @return {void}
+ */
+function setAlert() {
+  let alert = document.getElementById('alert');
+  alert.removeAttribute('hidden');
+  alert.innerHTML = `
+  <p>EL año 
+    <span class="badge text-bg-success">${anio}</span> del mes 
+    <span class="badge text-bg-success">${mes}</span> tiene
+    <span class="badge text-bg-success">${dias}</span> días
+  </p>
+  `;
+}
